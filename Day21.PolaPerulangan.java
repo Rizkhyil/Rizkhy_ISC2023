3b40edb264b4664971f04a64f611fc576dd90ec8
Rizkhyil <147228705+Rizkhyil@users.noreply.github.com>
2023-10-29 12:45:18 +0700

    Create Day21.PolaPerulangan.java

---
+++ Day21.PolaPerulangan.java
@@ -0,0 +1,21 @@
+package day21;
+import java.util.Scanner;
+public class Day21 {
+
+    public static void main(String[] args) {
+        Scanner rizz = new Scanner(System.in);
+        System.out.print(" Masukkan angka : ");
+        
+        int jumlah = rizz.nextInt();
+        for (int a = 1; a <= jumlah; a++) {
+            for (int i = 1; i <= 10; i++) {
+                System.out.print("x ");
+                
+            }
+            System.out.println("x ");
+            
+        }
+        System.out.println(" ");
+    }
+    
+}
