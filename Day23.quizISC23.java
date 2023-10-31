b65be0984b9ec8e743b15f062daa90c2371766bd
Rizkhyil <147228705+Rizkhyil@users.noreply.github.com>
2023-10-31 18:31:05 +0800

    Create Day23.quizISC23.java

---
+++ Day23.quizISC23.java
@@ -0,0 +1,15 @@
+package Day23;
+import java.util.Scanner;
+public class Main {
+    
+    public static void main(String[] args) {
+       Scanner rizz = new Scanner (System.in);
+        System.out.print(" Masukan  batas : ");
+        int batas = rizz.nextInt();
+        for (int a = 3; a<= batas; a+=3){
+            System.out.print( a+ " ");
+        }
+        
+            System.out.println(">_<");
+    }
+}
