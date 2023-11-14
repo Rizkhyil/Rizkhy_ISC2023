3088daaaabb18256e2ec94f6a44ecec22d0935be
Rizkhyil <147228705+Rizkhyil@users.noreply.github.com>
2023-11-14 22:33:44 +0800

    Create Day37.Array.java

---
+++ Day37.Array.java
@@ -0,0 +1,15 @@
+package Day37;
+public class Main{
+    
+    public static void main(String[] args) {
+        
+        int[] angka = {5, 4, 3, 2, 1};
+        
+        System.out.println(" Index 0    ="+angka[0]);
+        System.out.println(" Index I    ="+angka[1]);
+        System.out.println(" Index II   ="+angka[2]);
+        System.out.println(" Index III  ="+angka[3]);
+        System.out.println(" Index IV   ="+angka[4]);
+        
+    }
+}
