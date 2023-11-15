9d75212ff112ed5d6d01b361354617f325f4dbf7
Rizkhyil <147228705+Rizkhyil@users.noreply.github.com>
2023-11-15 19:54:30 +0800

    Create Day38.LopingArray.java

---
+++ Day38.LopingArray.java
@@ -0,0 +1,16 @@
+package Day38;
+public class Main{
+    
+    public static void main(String[] args) {
+        
+        int[] R = {7, 6, 5, 4, 3, 2, 1};
+        int jumlah = R.length;
+        for(int y = 0; y < jumlah; y++){
+            System.out.print(R[y]+" ");
+        }
+        
+        
+        
+        
+    }
+}
