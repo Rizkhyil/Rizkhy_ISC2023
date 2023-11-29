6a06c2bfba18a0c8a6f9539062ffcf674f46e837
Rizkhyil <147228705+Rizkhyil@users.noreply.github.com>
2023-11-30 00:41:50 +0800

    Create Day53.Cara2MultiArray.java

---
+++ Day53.Cara2MultiArray.java
@@ -0,0 +1,15 @@
+package Day53;
+public class Main {
+
+    public static void main(String[] args) {
+        
+        String[][] nama = {
+            {" Hera", " kholik", " Gedry"},
+            {" Aril", " Jaii", " Rian"}
+        };
+        System.out.println("~~~~~~~OutPut~~~~~~~~");
+        System.out.println("");
+        System.out.println("      "+nama[0][1]);
+        System.out.println("      "+nama[0][0]);
+    }
+}
