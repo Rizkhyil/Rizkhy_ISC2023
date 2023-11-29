e090a2e27a82278402e58f796986ea5cc19f9ef0
Rizkhyil <147228705+Rizkhyil@users.noreply.github.com>
2023-11-29 20:42:52 +0800

    Create Day52.MultiArray.java

---
+++ Day52.MultiArray.java
@@ -0,0 +1,18 @@
+package Day52;
+public class Main {
+
+    public static void main(String[] args) {
+        
+        String[][] r = new String[3][3];
+        
+        r [0][0] = "Hera";
+        r [0][1] = "kholil";
+        r [0][2] = "Gedry";
+        r [1][0] = "Aril";
+        r [1][1] = "Jaii";
+        r [1][2] = "Rian";
+        System.out.println("~~~~~~~OutPut~~~~~~~~");
+        System.out.println("");
+        System.out.println("        "+r[0][0]);
+    }
+}
