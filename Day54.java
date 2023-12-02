696d8d526f29877e737ff3391f9d35d9d94ac8c3
Rizkhyil <147228705+Rizkhyil@users.noreply.github.com>
2023-12-02 09:33:20 +0800

    Create Day54.java

---
+++ Day54.java
@@ -0,0 +1,15 @@
+package Day54;
+public class Main {
+
+    public static void main(String[] args) {
+        String[][] nama = {
+            {" Hera", " Kholil"," Gedry"},
+            {" K.lisa", " K.Afrisal"},
+            {" Aril", " Rian", " Jai", " Nama", " Teman ISC"}
+        };
+        System.out.println("~~~~~~~OutPut~~~~~~~");
+        System.out.println("");
+        System.out.println(nama.length);
+        System.out.println(nama[0].length);
+    }
+}
