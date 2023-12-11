d474d1eb759eb70dd20a7c79dad8cefd981a1253
Rizkhyil <147228705+Rizkhyil@users.noreply.github.com>
2023-12-11 23:37:55 +0800

    Create Day64.Prosedur.java

---
+++ Day64.Prosedur.java
@@ -0,0 +1,14 @@
+package Day64;
+import java.util.*;
+public class Main {
+    public static void Mahasiswa_2023 (){
+        System.out.println("kelas A : Aril ");
+        System.out.println("kelas B : Rayhan ");
+        System.out.println("kelas C : Rizkhy ");
+        System.out.println("kelas D : Kholil ");
+        System.out.println("kelas E : Renn ");
+    }
+    public static void main(String[] args) {
+        Mahasiswa_2023();
+    }   
+}
