8f15bbc09a73acf479e8883369aaace5505373b8
Rizkhyil <147228705+Rizkhyil@users.noreply.github.com>
2023-12-13 22:23:09 +0800

    Create Day66.Fungsi.java

---
+++ Day66.Fungsi.java
@@ -0,0 +1,13 @@
+package fungsi;
+public class Main{
+    
+    // Fungsi!!
+    public static String putri(String nama){
+        int umur = 19;
+        return nama+"  ~ "+umur+" Thn.";
+    }
+    public static void main(String[] args) {
+        System.out.println("~~~~~~~~~~~~~OutPut~~~~~~~~~~~~");
+        System.out.println(putri("Rizkhy"));
+    }   
+}
