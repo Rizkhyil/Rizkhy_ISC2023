d3aec143f60a4f201bd5dff6234ea7d51497e82c
Rizkhyil <147228705+Rizkhyil@users.noreply.github.com>
2023-12-15 21:55:47 +0800

    Create Day68.FunsiM.java

---
+++ Day68.FunsiM.java
@@ -0,0 +1,11 @@
+package Day68;
+public class Main{
+    public static String putra(String nama, int umur){
+        return nama+" umur saya"+umur+" thn. ";
+        
+    }
+    public static void main(String[] args) {
+        System.out.println("~~~~~~~~Output~~~~~~~~");
+        System.out.println(putra(" Rizkhy ", 19));
+    }   
+}
