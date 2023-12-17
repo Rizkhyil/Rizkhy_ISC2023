7297b80d73b8ebb19703171848855b03871f5a9c
Rizkhyil <147228705+Rizkhyil@users.noreply.github.com>
2023-12-17 22:44:47 +0800

    Create Day70.PalestinaArray.java

---
+++ Day70.PalestinaArray.java
@@ -0,0 +1,14 @@
+package Day70;
+public class Main{
+
+    public static void main(String[] args) {
+        String[] nama = {"Kami ", "berada ", "di pihak ", "Palestina!!"};
+        int data = nama.length;
+        System.out.println("~~~~~FREE FALESTINE~~~~~");
+        for(int i = 0; i < data; i++){
+            System.out.print(nama[i]);
+            
+        }
+        System.out.println("");
+    }   
+}
