f581d72d3f74bd00f29ff4e2d64d04d388b4ef5a
Rizkhyil <147228705+Rizkhyil@users.noreply.github.com>
2023-12-29 19:40:54 +0800

    Create Day81.MethodFungsi.java

---
+++ Day81.MethodFungsi.java
@@ -0,0 +1,11 @@
+package Fungsi;
+public class Main{
+    public static String data(){
+        String nama = "Rizkhy";
+        return nama;
+    }
+    public static void main(String[] args) {
+        System.out.println("~~~~~~~~~~~~~OutPut~~~~~~~~~~~~");
+        System.out.println(data());
+    }   
+}
