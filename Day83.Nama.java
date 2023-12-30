d30118dbb64c695cbd44874580a806f88f00c1f4
Rizkhyil <147228705+Rizkhyil@users.noreply.github.com>
2023-12-30 20:51:48 +0800

    Create Day83.Nama.java

---
+++ Day83.Nama.java
@@ -0,0 +1,9 @@
+package Fungsi;
+public class Main{
+    
+    public static void main(String[] args) {
+        String[] nama = {"Messi", "Ronaldo", "Neymar", "Kante", "Mane"};
+        System.out.println("~~~~~~~~~~~~~OutPut~~~~~~~~~~~~");
+        System.out.println(nama[0]+" Adalah fens dari "+nama[4]);
+    }   
+}
