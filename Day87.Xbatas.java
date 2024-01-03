cbf6ba3c570a495246f54ae19c204da705ceaecd
Rizkhyil <147228705+Rizkhyil@users.noreply.github.com>
2024-01-04 06:19:34 +0800

    Create Day87.Xbatas.java

---
+++ Day87.Xbatas.java
@@ -0,0 +1,12 @@
+package While;
+import java.util.*;
+public class Main{
+    public static void main(String[] args) {
+        int a = 0;
+        do{
+            
+            System.out.println("Lopp - "+a);
+            a++;
+        }while(true);
+    }
+}
