436c63e871864b8c0c6aa122c808e6d7dc5f9434
Rizkhyil <147228705+Rizkhyil@users.noreply.github.com>
2024-01-04 21:51:43 +0800

    Create Day88.java

---
+++ Day88.java
@@ -0,0 +1,18 @@
+package Arrays;
+public class Main {
+    public static void main (String args[]) {
+        
+        int[] R = {7, 18, 46, 33, 21, 39, 40} ;
+        int jumlah_index = R.length;
+        int max = R[0] ;
+        for(int y = 0; y < jumlah_index; y++) {
+            if(R[y] > max ) {
+                max = R[y];
+            }
+        }
+        System.out.println("~~~~~Out put~~~~~");
+        System.out.print("Max  = "+max);
+        
+        
+    }
+}
